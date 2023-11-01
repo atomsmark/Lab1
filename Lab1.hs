4700ed215b82e7b8517e03529941905e18476cba
atomsmark <agnes.tomsmark@gmail.com>
2023-11-01 16:35:57 +0000

    Got help

--- Lab1.hs
+++ Lab1.hs
@@ -23,7 +23,7 @@ stepsPower n k = k + 1
 
 power1 n k | k < 0 = error "power: negative argument"
 power1 n 0         = 1
-power1 n k         = product (replicate ((fromInteger k) n))   -- Behöver n fromInteger?
+power1 n k         = product (replicate (fromInteger k) n)   -- Behöver n fromInteger?
 
 
 -- C -------------------------
@@ -31,8 +31,8 @@ power1 n k         = product (replicate ((fromInteger k) n))   -- Behöver n fro
 
 power2 n k | k < 0  = error "power: negative argument"
 power2 n 0          = 1
-power2 n k | odd k  = n * power2 (n (k-1))
-           | even k = power2 ((n*n) (k 'div' 2))
+power2 n k | odd k  = n * (power2 n (k-1))
+           | even k = power2 (n*n) (k `div` 2)
 
 -- D -------------------------
 {- 
@@ -46,11 +46,14 @@ power2 n k | odd k  = n * power2 (n (k-1))
 
 -- 
 
-prop_powers n k = power (n k) == power1 (n k) == power2 (n k)
+prop_powers n k = power n k == power1 n k && power1 n k == power2 n k
 
 --
 powerTest :: Bool
-powerTest = undefined
+powerTest = prop_powers 3 8
+          && prop_powers 3 0
+          && prop_powers 3 3
+          && prop_powers 3 4
 
 --
 prop_powers' = undefined
